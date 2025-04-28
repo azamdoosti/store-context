5f1048a228ba22a3669e995bf8d25735e9222591
azam <a.doosti@gmail.com>
2025-04-29 01:21:49 +0330

    filtering products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,19 +1,50 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../components/Card'
 import {useProducts} from '../context/ProductContext'
 import styles from './ProductsPage.module.css'
 import Loader from '../components/Loader'
 import {ImSearch} from 'react-icons/im'
+import {FaListUl} from 'react-icons/fa'
+import { filterProducts, searchProducts } from '../helpers/helper'
 
 
 const ProductsPage = () => {
     const products = useProducts()
     const [search , setSearch] = useState("")
+    const [displayed , setDisplayed] = useState([])
+    const [query, setQuery] = useState({})
     console.log(products)
-    const searchHandler =()=>{
+   
+    useEffect(()=>{
+      setDisplayed(products)
+    },[products])
+
+
+     useEffect(()=>{
+      let finalProducts = searchProducts(products , query.search)
+      finalProducts= filterProducts(filterProducts , query.category)
+      setDisplayed(finalProducts)
+      console.log(finalProducts)
+     },[query])
+
+
+  const searchHandler =()=>{
+    setQuery(query=> ({...query, search:search}))
       console.log(search)
     }
+
+  const categoryHandler =(event)=>{
+  const {tagName} = event.target
+  console.log(tagName)
+  const category = event.target.innerText.toLowerCase()
+  console.log(category)
+  if(tagName!== LI) return;
+  setQuery((query)=> ({...query, category}))
+}
+ 
+
+
   return (
     <>
     <div>
@@ -24,12 +55,25 @@ const ProductsPage = () => {
     </div>
     <div className={styles.container}>
       <div className={styles.products}>
-        {!products.length && <Loader/>}
-        {products.map((product)=>{
+        {!displayed.length && <Loader/>}
+        {displayed.map((product)=>{
            return <Card key={product.id} data={product}/>
         })}
       </div>
-      <div>Sidebar</div>
+      <div>
+       <div>
+        <FaListUl/>
+        <p>Categories</p>
+       </div>
+       <ul onClick={categoryHandler}>
+        <li>All</li>
+        <li>Electronics</li>
+        <li>Jewelry</li>
+        <li>Men's Clothing</li>
+        <li>Women's Clothing</li>
+       </ul>
+      </div>
+      
     </div>
     </>
   )
